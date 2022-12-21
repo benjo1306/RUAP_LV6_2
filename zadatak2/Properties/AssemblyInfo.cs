ab57fc9b1b895d92a6d3a708b79a3cb446bc57fe
Nikola <mdamjanovic2010@gmail.com>
2022-12-21 13:40:19 +0100

    Run app

--- zadatak2/Properties/AssemblyInfo.cs
+++ zadatak2/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("drugi_zad")]
+[assembly: AssemblyTitle("zadatak2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("drugi_zad")]
+[assembly: AssemblyProduct("zadatak2")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5fcdd374-2aea-41ae-8afa-79c58766595e")]
+[assembly: Guid("f8e34283-bf54-46fa-b450-034ba2602bbf")]
 
 // Version information for an assembly consists of the following four values:
 //
